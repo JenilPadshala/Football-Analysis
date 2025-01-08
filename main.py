bf2eeca610b31a82cd83b29b3a315799aeb63670
Jenil Padshala <jenilpadshala@gmail.com>
2025-01-08 23:26:22 +0530

    Object tracking using ByteTrack from supervision

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from utils import read_video, save_video
-
+from trackers import Tracker
 
 def main():
     # Read video
     video_frames = read_video("input_videos/08fd33_4.mp4")
+
+    # Initialize tracker
+    tracker = Tracker("models/best.pt")
+
+    # Get object tracks
+    tracks = tracker.get_object_tracks(video_frames)
 
     #save video
     save_video(video_frames, "output_videos/output.avi")
